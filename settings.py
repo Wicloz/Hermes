e7069c11d05b16060a3e08d52f1827dfc2475595
Wilco de Boer <deboer.wilco@gmail.com>
2025-07-29 00:33:21 +0200

    Load `.env` file without external module

--- settings.py
+++ settings.py
@@ -1,12 +1,18 @@
 from sqlalchemy import create_engine
 from database import MyBase
-from os import getenv
-from dotenv import load_dotenv
+from os import environ
+
+
+def load_dotenv():
+    with open('.env', 'r') as fp:
+        for line in fp:
+            line = line.split('#', 1)[0]
+            if '=' in line:
+                key, value = line.split('=', 1)
+                environ[key.strip()] = value.strip()
 
 
 load_dotenv()
-
-
-ENGINE = create_engine(getenv('DATABASE_URL'))
+ENGINE = create_engine(environ.get('DATABASE_URL'))
 MyBase.metadata.create_all(ENGINE)
-TWITTER_TOKEN = getenv('TWITTER_TOKEN')
+TWITTER_TOKEN = environ.get('TWITTER_TOKEN')
